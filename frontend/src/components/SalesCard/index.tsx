650b62c96c7df1a5980e1b0329e1763075eac13a
Jonathas Lopes <jon.athas@hotmail.com>
2022-09-12 21:51:56 -0300

    axios, useEffect first request

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,14 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import NotificationButton from '../NotificationButton';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import './styles.css';
+import axios from 'axios';
 
 function SalesCard() {
     const date = new Date(new Date().setDate(new Date().getDate() - 365));
     
     const [from, setFrom] = useState(date);
     const [to, setTo] = useState(new Date());
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/sales").then(response => {
+            console.log(response.data);
+        });
+    }, []);
 
     return (
         <div className="dsmeta-card">
