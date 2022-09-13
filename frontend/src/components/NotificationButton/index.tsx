11fab1f9607445e4dcb9aca2c11567b1eb3fb355
Jonathas Lopes <jon.athas@hotmail.com>
2022-09-12 22:28:46 -0300

    send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,21 @@
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 import './styles.css';
+import axios from 'axios';
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function NotificationButton({ saleId }: Props) {
+    function handleClick(id: number) {
+        axios(`${BASE_URL}/sales/${id}/notification`).then((resp) => {
+
+        });
+    }
+
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
